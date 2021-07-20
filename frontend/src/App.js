c16d85b06739e56fd0e6c8c9fb08a86ace48d36d
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-07-20 23:49:09 +0530

    added frontend libraries and header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Header/>
+      <main style={{minHeight: "93vh"}}></main>
+      <Footer/>
     </div>
   );
 }
