06f2da97633df2324df96d25c02f53905dea3d62
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-07-22 01:46:37 +0530

    connected mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,25 @@
 const express = require("express");
-const notes = require('./data/notes');
-const dotenv = require('dotenv');
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+const { connect } = require("mongoose");
+const connectDB = require("./config/db");
 
 const app = express();
 dotenv.config();
-
-app.get('/', (req, res)=>{
-    res.send("API is running");
+connectDB();
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
 
-app.get('/api/notes', (req, res)=>{
-    res.json(notes);
-})
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
 
-app.get('/api/notes/:id', (req, res)=>{
-    const note = notes.find((n)=> n._id === req.params.id );
-    res.send(note);
-})
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.send(note);
+});
 
-const PORT = process.env.PORT  || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server Started on ${PORT}`));
