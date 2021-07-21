c8e5caa51aac962e964b656c255d53fab17fd201
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-07-21 22:59:55 +0530

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import LandingPage from './Screens/LandingPage/LandingPage'
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import LandingPage from "./Screens/LandingPage/LandingPage";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
+    <BrowserRouter>
+      <Header />
       <main>
-        <LandingPage/>
+        <LandingPage />
       </main>
-      <Footer/>
-    </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
