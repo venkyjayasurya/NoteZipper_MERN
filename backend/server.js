809f202c80e7df6f38aa32bdb4fadf1731fcab8a
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-07-23 00:59:42 +0530

    Completed backend API

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 // Include notes static database
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
-const { connect } = require("mongoose");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware")
 
 // Initialize express app
 const app = express();
+app.use(express.json());
 
 // Configure dotenv to use in the file
 dotenv.config();
@@ -23,10 +25,16 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.get("/api/notes/:id", (req, res) => {
-// returns only a single note which is passed in url parameters
+  // returns only a single note which is passed in url parameters
   const note = notes.find((n) => n._id === req.params.id);
   res.send(note);
 });
+
+// Route
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 // Use process.env to access the .env variables, if it is not accessed we are setting the PORT to 5000
 const PORT = process.env.PORT || 5000;
