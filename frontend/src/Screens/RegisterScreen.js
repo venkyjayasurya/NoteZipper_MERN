b112b0e50a06763cebff412cb02ebf654ceea104
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-07-23 23:56:57 +0530

    added cloudinary service

--- frontend/src/Screens/RegisterScreen.js
+++ frontend/src/Screens/RegisterScreen.js
@@ -1,6 +1,9 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import ErrorMessage from "../components/ErrorMessage";
+import Loading from "../components/Loading";
 import MainScreen from "../components/MainScreen";
 
 const RegisterScreen = () => {
@@ -16,24 +19,73 @@ const RegisterScreen = () => {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
-
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
-    console.log(email, name)
-    //   if (password !== confirmpassword) {
-    //     setMessage("Passwords do not match");
-    //   } else dispatch(register(name, email, password, pic));
+    console.log(email, name);
+    if (password !== confirmPassword) {
+      setMessage("Passwords do not match");
+    } else {
+      setMessage(null);
+      try {
+        const config = {
+          headers: {
+            "Content-type": "application/json",
+          },
+        };
+        setLoading(true);
+        const { data } = await axios.post(
+          "http://localhost:5000/api/users",
+          { name, pic, email, password },
+          config
+        );
+        setLoading(false);
+        localStorage.setItem("userInfo", JSON.stringify(data))
+      } catch (error) {
+        setError(error.response.data.message)
+        setLoading(false);
+      }
+    }
   };
+
+const postDetails = (pics) =>{
+    if(!pics){
+        return setPicMessage("Please upload an image!")
+    }
+    setPicMessage(null)
+
+    if(pics.type === 'image/jpeg' || pics.type === 'image/png' || pics.type === 'image/jpg'){
+        const data = new FormData()
+        data.append('file', pics)
+        data.append('upload_preset', 'notezipper')
+        data.append('cloud_name', 'dup5iwodq')
+        fetch("https://api.cloudinary.com/v1_1/dup5iwodq/image/upload", {
+            method: "POST",
+            body: data,
+        }).then((res)=> res.json())
+        .then((data)=>{
+            console.log(data)
+            setPic(data.url.toString())
+        }).catch((err) => {
+            console.log(err)
+        })
+    }else{
+        return setPicMessage("Please Select proper file format image")
+    }
+}
+
 
   return (
     <MainScreen title="REGISTER">
       <div className="loginContainer">
+        {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+        {message && <ErrorMessage variant="danger">{message}</ErrorMessage>}
+        {loading && <Loading/>}
         <Form onSubmit={submitHandler}>
           <Form.Group className="mb-3" controlId="name">
             <Form.Label>Name</Form.Label>
             <Form.Control
               type="string"
-              value={email}
+              value={name}
               placeholder="Enter Name"
               onChange={(e) => setName(e.target.value)}
             />
@@ -63,16 +115,20 @@ const RegisterScreen = () => {
             <Form.Label>Confirm Password</Form.Label>
             <Form.Control
               type="password"
-              value={password}
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
+              value={confirmPassword}
+              placeholder="Confirm Password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
             />
           </Form.Group>
+
+            {picMessage && (
+                <ErrorMessage variant="danger">{picMessage}</ErrorMessage>
+            )}
 
           <Form.Group controlId="pic">
             <Form.Label>Profile Picture</Form.Label>
             <Form.File
-                // onChange={(e) => postDetails(e.target.files[0])}
+              onChange={(e) => postDetails(e.target.files[0])}
               id="custom-file"
               type="image/png"
               label="Upload Profile Picture"
